da74ddeda8945c09ce5cb615e416a0a120908b97
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2024-01-14 12:56:02 +0800

    init project
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    QObject::connect(
+            &engine,
+            &QQmlApplicationEngine::objectCreationFailed,
+            &app,
+            []() { QCoreApplication::exit(-1); },
+            Qt::QueuedConnection);
+    engine.loadFromModule("tc_server_steam", "Main");
+
+    return app.exec();
+}
---
+++ test.qml
@@ -0,0 +1,15 @@
+import QtQuick
+
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Hello World")
+
+    Rectangle {
+        width: 100;
+        height: 100;
+        color: "#090989";
+    }
+
+}
