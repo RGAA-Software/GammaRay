ffc843312ec68217de8ff0476856118e13cd4d31
hy <hy@gmail.com>
2024-04-18 11:16:41 +0800

    check driver version
    Signed-off-by: hy <hy@gmail.com>

--- src/system_monitor.cpp
+++ src/system_monitor.cpp
@@ -9,6 +9,9 @@
 #include "tc_controller/vigem/sdk/ViGEm/Client.h"
 #include "gr_context.h"
 
+#pragma comment(lib, "version.lib")
+#pragma comment(lib, "kernel32.lib")
+
 namespace tc
 {
 
@@ -23,29 +26,20 @@ namespace tc
     void SystemMonitor::Start() {
         vigem_driver_manager_ = VigemDriverManager::Make();
 
+        if (!CheckViGEmDriver()) {
+            // install it
+        }
+
         monitor_thread_ = std::make_shared<Thread>([=, this]() {
             while (!exit_) {
-                // 1. checking vigem driver is installed or not
-                bool detect_ok = false;
-                if (!vigem_driver_manager_->IsVigemDriverInstalled()) {
-                    detect_ok = vigem_driver_manager_->Detect();
-                }
-                // 2. driver is not exist, to install
-                if (vigem_driver_manager_->IsVigemDriverInstalled() && detect_ok) {
-                    // installed & running
-                    LOGI("Installed and running");
+
+                bool vigem_installed = CheckViGEmDriver();
+                if (vigem_installed) {
+                    if (!TryConnectViGEmDriver()) {
+                        // Connect error
+                    }
                 } else {
-                    // 3. driver seems already exists, try to connect
-                    if (!connect_vigem_success_) {
-                        connect_vigem_success_ = vigem_driver_manager_->TryConnect();
-                    }
-                    // 4. connect failed, to install
-                    if (!connect_vigem_success_) {
-                        LOGI("connect failed.");
-                        vigem_driver_manager_->InstallViGem();
-                    } else {
-                        LOGI("already tested, connect to vigem success.");
-                    }
+                    // not installed
                 }
 
                 LOGI("system checking ...");
@@ -58,6 +52,71 @@ namespace tc
         exit_ = true;
         if (monitor_thread_->IsJoinable()) {
             monitor_thread_->Join();
+        }
+    }
+
+    bool SystemMonitor::CheckViGEmDriver() {
+        DWORD major = 0, minor = 0;
+        std::wstring path = L"C:\\Windows\\System32\\drivers\\ViGEmBus.sys";
+
+        if (GetFileVersion(path, major, minor)) {
+            LOGI("ViGEmBus: {}.{}", major, minor);
+            if (major > 1 || (major == 1 && minor >= 17)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    bool SystemMonitor::GetFileVersion(const std::wstring& filePath, DWORD& major, DWORD& minor){
+        DWORD dummy;
+        DWORD size = GetFileVersionInfoSizeW(filePath.c_str(), &dummy);
+        if (size == 0) {
+            return false;
+        }
+
+        std::vector<BYTE> data(size);
+        if (!GetFileVersionInfoW(filePath.c_str(), 0, size, data.data())) {
+            return false;
+        }
+
+        VS_FIXEDFILEINFO* fileInfo = nullptr;
+        UINT fileInfoSize;
+        if (!VerQueryValueW(data.data(), L"\\", reinterpret_cast<void**>(&fileInfo), &fileInfoSize)) {
+            return false;
+        }
+
+        major = (fileInfo->dwFileVersionMS >> 16) & 0xffff;
+        minor = (fileInfo->dwFileVersionLS >> 16) & 0xffff;
+        return true;
+    }
+
+    bool SystemMonitor::TryConnectViGEmDriver() {
+        // 1. checking vigem driver is installed or not
+        bool detect_ok = false;
+        if (!vigem_driver_manager_->IsVigemDriverInstalled()) {
+            detect_ok = vigem_driver_manager_->Detect();
+        }
+
+        // 2. driver is not exist, to install
+        if (vigem_driver_manager_->IsVigemDriverInstalled() && detect_ok) {
+            // installed & running
+            LOGI("Installed and running");
+        } else {
+            // 3. driver seems already exists, try to connect
+            if (!connect_vigem_success_) {
+                connect_vigem_success_ = vigem_driver_manager_->TryConnect();
+            }
+            // 4. connect failed, to install
+            if (!connect_vigem_success_) {
+                LOGI("connect failed.");
+            } else {
+                LOGI("already tested, connect to vigem success.");
+                return true;
+            }
         }
     }
 
