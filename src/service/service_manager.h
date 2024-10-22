7ff4b4e4d39283464ce87d805bdb0b2ef0159e71
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2024-10-22 18:22:00 +0800

    test service
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

---
+++ src/service/service_manager.h
@@ -0,0 +1,44 @@
+//
+// Created by RGAA on 22/10/2024.
+//
+
+#ifndef GAMMARAY_SERVICE_MANAGER_H
+#define GAMMARAY_SERVICE_MANAGER_H
+
+#include <memory>
+#include <string>
+
+namespace tc
+{
+    enum class ServiceStatus {
+        kUnknownStatus,
+        kPending,
+        kRunning,
+        kStopped,
+    };
+
+    class ServiceManager {
+    public:
+        static std::shared_ptr<ServiceManager> Make();
+        ServiceManager();
+
+        void Init(const std::string& srv_name, const std::string& path, const std::string& display_name, const std::string& description);
+        void Install();
+        void Start();
+        void Stop();
+        void Remove();
+        void RemoveImmediately();
+        ServiceStatus QueryStatus();
+
+        static std::string StatusAsString(ServiceStatus status);
+
+    private:
+        std::string srv_name_;
+        std::string srv_exe_path_;
+        std::string srv_display_name_;
+        std::string srv_description_;
+    };
+
+}
+
+#endif //GAMMARAY_SERVICE_MANAGER_H
