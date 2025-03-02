4258cdc1b82dc7bed8860c92c753424b76e3c155
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-03-02 11:46:26 +0800

    update package script
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- package/package_rel_debug.bat
+++ package/package_rel_debug.bat
@@ -1,3 +1,5 @@
+rmdir /s /q packages\com.rgaa.gammaray\data
+
 python install.py rel-debug
 
 binarycreator.exe -c config/config.xml -p packages GammaRaySetup_DebugInfo.exe -v
--- package/package_release.bat
+++ package/package_release.bat
@@ -1,3 +1,5 @@
+rmdir /s /q packages\com.rgaa.gammaray\data
+
 python install.py release
 
 binarycreator.exe -c config/config.xml -p packages GammaRaySetup.exe -v
