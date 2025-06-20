55fd4580dee9bdd1395ad8f216273cf5870e91dd
ChengduDreamer <chengduwiwi@gmail.com>
2025-06-20 09:17:03 +0800

    add clear_icons_cache.bat

---
+++ package/clear_icons_cache.bat
@@ -0,0 +1,28 @@
+rem 关闭Windows外壳程序explorer
+
+taskkill /f /im explorer.exe
+
+rem 清理系统图标缓存数据库
+
+attrib -h -s -r "%userprofile%\AppData\Local\IconCache.db"
+
+del /f "%userprofile%\AppData\Local\IconCache.db"
+
+attrib /s /d -h -s -r "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\*"
+
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_32.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_96.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_102.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_256.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_1024.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_idx.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_sr.db"
+
+rem 清理 系统托盘记忆的图标
+
+echo y|reg delete "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v IconStreams
+echo y|reg delete "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v PastIconsStream
+
+rem 重启Windows外壳程序explorer
+
+start explorer
