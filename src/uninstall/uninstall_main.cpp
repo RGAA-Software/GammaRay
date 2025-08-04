a86b4eba5d5f999f3b9d11e983d192ec6f15e98c
ChengduDreamer <chengduwiwi@gmail.com>
2025-08-04 10:29:49 +0800

    Improve uninstallation

--- src/uninstall/uninstall_main.cpp
+++ src/uninstall/uninstall_main.cpp
@@ -9,14 +9,23 @@
 #include <QPushButton>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
-#include <QProcess>
 #include <QTimer>
+#include <thread>
+#include <chrono>
 #include <windows.h>
+#include "tc_common_new/win32/process_helper.h"
+#include "tc_common_new/log.h"
 #include "service/service_manager.h"
 #include "tc_qt_widget/sized_msg_box.h"
 #include "gflags/gflags.h"
 
 using namespace tc;
+
+static const std::string kGammaRayName = "GammaRay.exe";
+static const std::string kGammaRayGuardName = "GammaRayGuard.exe";
+static const std::string kGammaRayRenderName = "GammaRayRender.exe";
+static const std::string kGammaRayClientInner = "GammaRayClientInner.exe";
+static const std::string kGammaRayService = "GammaRayService.exe";
 
 DEFINE_string(option, "null", "uninstall/stop");
 
@@ -30,7 +39,45 @@ int main(int argc, char** argv) {
     service_manager->Init("GammaRayService", "", "GammaRat Service", "** GammaRay Service **");
     
     if (FLAGS_option == "uninstall") {
-        service_manager->Remove(true);
+        service_manager->Remove(true);    
+        std::thread task_thread =  std::thread([=]() {
+           
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+            auto processes = tc::ProcessHelper::GetProcessList(false);
+            for (auto& process : processes) {
+                if (process->exe_full_path_.find(kGammaRayGuardName) != std::string::npos) {
+                    LOGI("Kill exe: {}", process->exe_full_path_);
+                    tc::ProcessHelper::CloseProcess(process->pid_);
+                    break;
+                }
+            }
+            for (auto& process : processes) {
+                if (process->exe_full_path_.find(kGammaRayClientInner) != std::string::npos) {
+                    LOGI("Kill exe: {}", process->exe_full_path_);
+                    tc::ProcessHelper::CloseProcess(process->pid_);
+                    break;
+                }
+            }
+            for (auto& process : processes) {
+                if (process->exe_full_path_.find(kGammaRayRenderName) != std::string::npos) {
+                    LOGI("Kill exe: {}", process->exe_full_path_);
+                    tc::ProcessHelper::CloseProcess(process->pid_);
+                    break;
+                }
+            }
+            for (auto& process : processes) {
+                if (process->exe_full_path_.find(kGammaRayName) != std::string::npos) {
+                    LOGI("Kill exe: {}", process->exe_full_path_);
+                    tc::ProcessHelper::CloseProcess(process->pid_);
+                }
+            }
+        });
+
+        if (task_thread.joinable()) {
+            task_thread.join();
+        }
+
         QString path = QCoreApplication::applicationDirPath();
         path += "/shadow_deleter.exe";
         auto process = new QProcess();
@@ -48,65 +95,5 @@ int main(int argc, char** argv) {
     else {
         return 0;
     }
-
-#if 0
-    auto service_manager = ServiceManager::Make();
-    service_manager->Init("GammaRayService", "", "GammaRat Service", "** GammaRay Service **");
-
-    QWidget widget;
-    widget.setWindowTitle("UNINSTALL");
-    widget.setFixedSize(480, 320);
-    auto root_layout = new QHBoxLayout();
-    auto layout = new QVBoxLayout();
-    root_layout->addLayout(layout);
-    widget.setLayout(root_layout);
-    auto btn_size = QSize(200, 40);
-    layout->addStretch();
-    // stop all
-    {
-        auto btn_stop = new QPushButton();
-        btn_stop->setFixedSize(btn_size);
-        QObject::connect(btn_stop, &QPushButton::clicked, &widget, [=]() {
-            auto mbox = SizedMessageBox::MakeOkCancelBox("STOP All Processes", "Do you want to stop all relative processes?");
-            if (mbox->exec() == 0) {
-                service_manager->Remove(false);
-            }
-        });
-        btn_stop->setText("STOP ALL");
-        layout->addWidget(btn_stop);
-
-    }
-
-    layout->addSpacing(20);
-
-    // uninstall button
-    {
-        auto btn_uninstall = new QPushButton();
-        btn_uninstall->setFixedSize(btn_size);
-        QObject::connect(btn_uninstall, &QPushButton::clicked, &widget, [=]() {
-            auto mbox = SizedMessageBox::MakeOkCancelBox("** UnInstall **", "Do you want to  UNINSTALL the software?");
-            if (mbox->exec() == 0) {
-                service_manager->Remove(true);
-                QTimer::singleShot(1000, [=]() {
-                    QString path = QCoreApplication::applicationDirPath();
-                    path += "/shadow_deleter.exe";
-                    auto process = new QProcess();
-                    QStringList args;
-                    args.append(path);
-                    process->startDetached(path, args);
-
-                    qApp->exit(0);
-                });
-            }
-        });
-        btn_uninstall->setText("UNINSTALL");
-        layout->addWidget(btn_uninstall);
-    }
-
-    layout->addSpacing(20);
-
-    widget.show();
-#endif
-
     return 0;
 }
