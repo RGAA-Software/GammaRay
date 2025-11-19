40ad97f440f50d1554617d06978908755b9ff890
Hector <hollow.ichigo.yoo@gmail.com>
2025-11-19 14:34:56 +0800

    fix building
    Signed-off-by: Hector <hollow.ichigo.yoo@gmail.com>

--- env_settings.cmake
+++ env_settings.cmake
@@ -1,4 +1,4 @@
 set(VCPKG_ROOT C:/source/vcpkg)
-set(QT_ROOT C:/qt6.8/6.8.3/msvc2022_64)
+set(QT_ROOT C:/Qt6.8.3/6.8.3/msvc2022_64)
 #set(VK_SDK C:/VulkanSDK/1.3.275.0/Include)
 set(VK_SDK_ROOT C:/VulkanSDK/1.3.290.0)
