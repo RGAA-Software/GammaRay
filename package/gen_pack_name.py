bd80396846ef465fca790aba2243db8d9704cf21
ChengduDreamer <chengdudreamer@gmail.com>
2025-11-25 13:44:41 +0800

    gen save pdb file name

--- package/gen_pack_name.py
+++ package/gen_pack_name.py
@@ -12,3 +12,7 @@ def extract_project_version(file_path):
 def gen_package_name() ->str:
     target_name = "GammaRay_" + extract_project_version("../version_config.h")
     return target_name
+
+def gen_package_pdb_name() ->str:
+    target_name = "GammaRay_pdb_" + extract_project_version("../version_config.h")
+    return target_name
